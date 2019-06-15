9b5a00c45d8bd2a5fb6530957afc73c02d5a06cd
Hussein Flaifel <husseinflaifel@Husseins-MacBook-Pro.local>
2019-06-15 15:10:17 +0300

    initial commit.

---
+++ testForAnas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testForAnas
+//
+//  Created by Hussein Flaifel on 6/15/19.
+//  Copyright © 2019 Hussein Flaifel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
